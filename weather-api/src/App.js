5bae67db262e9a18bff2784265a3f2c7b1e9dadb
Maya <maya.atiah.99@outlook.com>
2022-12-20 15:23:13 +0200

    Editing the App,js

--- weather-api/src/App.js
+++ weather-api/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './Header'
+import Middle from './Middle';
+import Lower from './Lower';
 
 function App() {
   return (
     <div className="App">
-       
+       <Header />
+    <Middle />
+    <Lower />
     </div>
   );
 }
