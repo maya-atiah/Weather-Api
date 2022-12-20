6008d13692b9f4487eecac92bd35f00f11400aa6
Mohamad Moughnie <mhdmoughnie@gmail.com>
2022-12-20 12:30:39 +0200

    images fixed

--- weather-api/src/Lower.js
+++ weather-api/src/Lower.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import cloudy from "./images/weather-icons/cloudy.svg";
+import clear from "./images/weather-icons/clear.svg";
 
 export default function Lower (){
     return(
@@ -7,37 +8,37 @@ export default function Lower (){
         <div className="lower-block">
             <div>
                 <span>03:00</span>
-<img  src="https://previews.123rf.com/images/ketle/ketle1901/ketle190100096/121995917-sun-cloud-icon-element-simple-app-isolated-symbol-on-blue-background-icon-cloudy-weather-flat-design.jpg"></img>
+             <img src={cloudy} alt="cloudy icon" />
                 <span>8 C</span>
             </div>
             <div>
                 <span>06:00</span>
-                <img  src="https://previews.123rf.com/images/ketle/ketle1901/ketle190100096/121995917-sun-cloud-icon-element-simple-app-isolated-symbol-on-blue-background-icon-cloudy-weather-flat-design.jpg"></img>
-                <span>9 C</span>
+                <img src={clear} alt="cloudy icon" />
+                 <span>9 C</span>
             </div>
             <div>
                 <span>09:00</span>
-                <img  src="https://previews.123rf.com/images/ketle/ketle1901/ketle190100096/121995917-sun-cloud-icon-element-simple-app-isolated-symbol-on-blue-background-icon-cloudy-weather-flat-design.jpg"></img>
-                <span>14 C</span>
+                <img src={clear} alt="cloudy icon" />
+                  <span>14 C</span>
             </div>
             <div>
                 <span>12:00</span>
-                <img  src="https://previews.123rf.com/images/ketle/ketle1901/ketle190100096/121995917-sun-cloud-icon-element-simple-app-isolated-symbol-on-blue-background-icon-cloudy-weather-flat-design.jpg"></img>
-                <span>17 C</span>
+                <img src={clear} alt="cloudy icon" />
+                  <span>17 C</span>
             </div>
             <div>
                 <span>15:00</span>
-                <img  src="https://previews.123rf.com/images/ketle/ketle1901/ketle190100096/121995917-sun-cloud-icon-element-simple-app-isolated-symbol-on-blue-background-icon-cloudy-weather-flat-design.jpg"></img>
-                <span>18 C</span>
+                <img src={clear} alt="cloudy icon" />
+                 <span>18 C</span>
             </div>
             <div>
                 <span>18:00</span>
-                <img  src="https://previews.123rf.com/images/ketle/ketle1901/ketle190100096/121995917-sun-cloud-icon-element-simple-app-isolated-symbol-on-blue-background-icon-cloudy-weather-flat-design.jpg"></img>
-                <span>16 C</span>
+                <img src={clear} alt="cloudy icon" />
+                 <span>16 C</span>
             </div>
             <div>
                 <span>21:00</span>
-                <img  src="https://previews.123rf.com/images/ketle/ketle1901/ketle190100096/121995917-sun-cloud-icon-element-simple-app-isolated-symbol-on-blue-background-icon-cloudy-weather-flat-design.jpg"></img>
+                <img src={cloudy} alt="cloudy icon" />
                 <span>13 C</span>
             </div>
             </div>
